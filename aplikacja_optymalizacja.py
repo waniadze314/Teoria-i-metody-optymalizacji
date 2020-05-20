267658070713e97ace464927532338e96a3e46f9
waniadze314 <kamilkasjan@poczta.fm>
2020-05-20 14:17:15 +0200

    add points ploting

--- aplikacja_optymalizacja.py
+++ aplikacja_optymalizacja.py
@@ -8,6 +8,11 @@ Created on Mon May 11 14:52:10 2020
 
 import PySimpleGUI as sg
 import tps
+from subprocess import *
+from io import StringIO  # Python3
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Design pattern 2 - First window does remain active
 st = tps.two_phase_simplex()
@@ -96,26 +101,117 @@ while True:
 
     if ev1 == 'Rozpocznij proces optymalizacji' and not win2_active:
         # get coefs
+        # Store the reference, in case you want to show things again in standard output
+ 
+        old_stdout = sys.stdout
+ 
+# This variable will store everything that is sent to the standard output
+ 
+        result = StringIO()
+        sys.stdout = result
+        
         st.get_problem_coefficients(vals1, ograniczenia)
         tables_to_print, function_points, optimal_value = st.calculate_table()
-        print(function_points)
-        print(optimal_value)
-        print(tables_to_print)
+        
+        # Redirect again the std output to screen
+ 
+        sys.stdout = old_stdout
+ 
+# Then, get the stdout like a string and process it!
+ 
+        result_string = result.getvalue()
+  
+        #print(function_points)
+        #print(optimal_value)
+        #print(tables_to_print)
 
         win2_active = True
         layout2 = [ 
         [sg.Text('PL – metoda dwufazowa simpleks', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
         [ sg.Text('Tablice simplexowe')],
-        [sg.Multiline('Tutaj zostana wyswietlone kolejne kroki algorytmu',size=(70,8))],
-        [sg.Text('Rozwiazanie optymalne'), sg.Text(' '*42), sg.Text('Optymalizowana funckja')],
-        [sg.Multiline('Tutaj pojawi się wynik optymalizacji', size=(37,1)), sg.Multiline('Tutaj wyswietli się optymalizowana funckja', size=(37,1))],
+        [sg.Multiline(key = 'tablice_simp',size=(70,8), disabled = True, autoscroll = True )],
+        [sg.Text('Rozwiazanie optymalne'), sg.Text(' '*42), sg.Text('Punkty funkcji')],
+        [sg.Multiline(key='optymalne', size=(37,1)), sg.Multiline(key = 'pkt_funkcji', size=(37,1))],
         [sg.Button('Cofnij'), sg.Button('Narysuj wykres')],
         ]
         win2 = sg.Window('Metoda simpleks - Wyniki obliczeń').Layout(layout2)
 
     if win2_active:
         ev2, vals2 = win2.Read(timeout=100)
+        win2['tablice_simp'].update(result_string)
+        win2['optymalne'].update(optimal_value)
+        win2['pkt_funkcji'].update(function_points)
+        #print(function_points[2][0], function_points[2][1])
         if ev2 is None or ev2 == 'Cofnij':
             win2.Close()
             win2_active = False
+        if ev2 is None or ev2 == 'Narysuj wykres':
+            
+            if ograniczenia == 1:
+                zasieg = int(vals1[11])
+                #print(zasieg)
+                x1 = np.linspace(0, zasieg)
+                plt.plot(x1, (int(vals1[11]) - int(vals1[5]) * x1)/int(vals1[6]))
+                plt.plot(0,0, 'co', label = '0')
+               #plt.plot(function_points[0][0], function_points[0][1], 'bo', label = '1')
+            if ograniczenia == 2:
+                zasieg = max(int(vals1[11]), int(vals1[18]))
+                #print(zasieg)
+                x1 = np.linspace(0, zasieg)
+                plt.plot(x1, (int(vals1[11]) - int(vals1[5]) * x1)/int(vals1[6]))
+                plt.plot(x1, (int(vals1[18]) - int(vals1[12])*x1)/int(vals1[13]))
+                plt.plot(0,0, 'co', label = '0')
+                #plt.plot(function_points[0][0], function_points[0][1], 'bo', label = '1')
+                #plt.plot(function_points[1][0], function_points[1][1], 'go', label ='2')
+            if ograniczenia == 3:
+                zasieg = max(int(vals1[11]), int(vals1[18]), int(vals1[25]))
+                #print(zasieg)
+                x1 = np.linspace(0, zasieg)
+                plt.plot(x1, (int(vals1[11]) - int(vals1[5]) * x1)/int(vals1[6]))
+                plt.plot(x1, (int(vals1[18]) - int(vals1[12])*x1)/int(vals1[13]))
+                plt.plot(x1, (int(vals1[25]) - int(vals1[19])*x1)/int(vals1[20]))
+                plt.plot(0,0, 'co', label = '0')
+                plt.plot(function_points[0][0], function_points[0][1], 'bo', label = '1')
+                plt.plot(function_points[1][0], function_points[1][1], 'go', label = '2')
+                plt.plot(function_points[2][0], function_points[2][1], 'ko', label = '3')
+            if ograniczenia == 4:
+                zasieg = max(int(vals1[11]), int(vals1[18]), int(vals1[25]))
+                #print(zasieg)
+                x1 = np.linspace(0, zasieg)
+                plt.plot(x1, (int(vals1[11]) - int(vals1[5]) * x1)/int(vals1[6]))
+                plt.plot(x1, (int(vals1[18]) - int(vals1[12])*x1)/int(vals1[13]))
+                plt.plot(x1, (int(vals1[25]) - int(vals1[19])*x1)/int(vals1[20]))
+                plt.plot(x1, (int(vals1[32]) - int(vals1[26])*x1)/int(vals1[27]))
+                plt.plot(0,0, 'co', label = '0')
+                plt.plot(function_points[0][0], function_points[0][1], 'bo', label = '1')
+                plt.plot(function_points[1][0], function_points[1][1], 'go', label = '2')
+                plt.plot(function_points[2][0], function_points[2][1], 'ko', label = '3')
+                #plt.plot(function_points[3][0], function_points[3][1], 'ro', label = '4')
+            if ograniczenia == 5:
+                zasieg = max(int(vals1[11]), int(vals1[18]), int(vals1[25]))
+                #print(zasieg)
+                x1 = np.linspace(0, zasieg)
+                plt.plot(x1, (int(vals1[11]) - int(vals1[5]) * x1)/int(vals1[6]))
+                plt.plot(x1, (int(vals1[18]) - int(vals1[12])*x1)/int(vals1[13]))
+                plt.plot(x1, (int(vals1[25]) - int(vals1[19])*x1)/int(vals1[20]))
+                plt.plot(x1, (int(vals1[32]) - int(vals1[26])*x1)/int(vals1[27]))
+                plt.plot(x1, (int(vals1[39]) - int(vals1[33])*x1)/int(vals1[34]))
+                plt.plot(0,0, 'co',  label = '0')
+                plt.plot(function_points[0][0], function_points[0][1], 'bo', label = '1')
+                plt.plot(function_points[1][0], function_points[1][1], 'go', label = '2')
+                plt.plot(function_points[2][0], function_points[2][1], 'ko', label = '3')
+                #plt.plot(function_points[3][0], function_points[3][1], 'ro', label = '4')
+                #plt.plot(function_points[4][0], function_points[4][1], 'mo', label = '5')
+
+            plt.ylim([0, zasieg])
+            plt.xticks(np.arange(0, zasieg))
+            plt.yticks(np.arange(0, zasieg))
+            plt.axhline(y=0, color='k')
+            plt.axvline(x=0, color='k')
+            plt.xlabel('x1')
+            plt.ylabel('x2')
+            plt.legend()
+            plt.show()
 win1.close()
+
+
